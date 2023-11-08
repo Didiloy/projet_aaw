48561cd3327646f67705d419e14c49407e12dbba
Zaiphyr <julientribouillois37240@gmail.com>
2023-11-08 17:37:45 +0100

    crud_db

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 require('dotenv').config()
+import {readUser, createUser} from "./database/script"
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
@@ -18,7 +19,24 @@ app.use(express.json());
 app.use(express.static(DIST_DIR));
 app.use(express.static(PUBLIC_DIR));
 
+//======= User ======
 
+/*
+app.get('/api/get-users', function(req, res){ //donne tous les utilisateurs 
+});
+*/
+
+app.get('/api/get-user/:username', function(req, res){ //donne un utilisateur
+   const usern = String(req.params.username)
+   const user = readUser(usern)
+   res.json(user)
+});
+/*
+app.post('/api/create-user', function(req, res){
+
+} )
+
+*/
 
 app.get("*", (req, res) => {
    res.sendFile(HTML_FILE, function(err){
