dfedeb92fc8db8097e84b09ac169f02e0a462469
dylan <didiloy2001@gmail.com>
2023-11-08 15:39:35 +0100

    added dotenv module. a .env file need to be created and is not versionned

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
@@ -10,14 +11,16 @@ app.listen(port, () => {
 
 const DIST_DIR = path.join(__dirname, "..", "client", "dist");
 const PUBLIC_DIR = path.join(__dirname, "..", "client", "public");
-const HTML_FILE = path.join(DIST_DIR, "index.html");
+const HTML_FILE = path.join(process.env.ENVIRONMENT == "dev" ? PUBLIC_DIR : DIST_DIR, "index.html");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(DIST_DIR));
 app.use(express.static(PUBLIC_DIR));
 
-app.get("/", (req, res) => {
+
+
+app.get("*", (req, res) => {
    res.sendFile(HTML_FILE, function(err){
       if(err){
          res.status(500).send(err);
