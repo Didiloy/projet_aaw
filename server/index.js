96a611e5ddaa2d7d5c0e1c1457f6accf904f2a05
KillianLesp <115100235+9MoiioM9@users.noreply.github.com>
2023-11-10 15:56:15 +0100

    Function deleteUser + allFavorite
    f

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
-const { readUser, createUser } = require("./database/script.js");
+const { readUser, createUser, deleteUser, createFavorite, readFavorite, updateFavorite, deleteFavorite } = require("./database/script.js");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
@@ -46,6 +46,70 @@ app.post("/api/create-user", async function (req, res) {
   console.log(token);
   console.log(usern);
   res.json(user);
+});
+
+app.delete("/api/delete-user/:username", async function (req, res){
+  const usern = req.body.username;
+  const response = {isDeleted : true};
+  try {
+    const user = await deleteUser(usern);
+    response.message = "User deleted with success !"
+    res.status(200).json(response);
+    
+    res.json(response);
+  }catch (err){
+    req.log.error(err);
+    response.message = "Error during deleting user  \n" + err;
+    response.isDeleted = false;
+
+    res.status(500).json(response);
+  }
+});
+
+//Favorite Management
+app.post("/api/create-favorite", async function (req, res){
+  //creation of Favorite
+  const idfav = req.body.id;
+  const quote = req.body.quoteId;
+  const user = req.body.userId;
+  const favorite = await createFavorite(idfav, quote, user);
+  //phase de test (a suppr plus tard)
+  console.log(favorite);
+  console.log(idfav);
+  console.log(quote);
+  console.log(user);
+
+  res.json(favorite);
+}); 
+
+app.get("/api/get-favorite/:id", async function(req, res){
+  const idfav = req.body.id;
+  const favorite = await readFavorite(idfav);
+  console.log(favorite);
+  res.json(favorite);
+});
+
+app.post("/api/update-favorite/:id", async function(req, res){
+  const quote = req.body.quoteId;
+  const user = req.body.userId;
+  const favorite = await updateFavorite(id, quote, user);
+  console.log(favorite);
+  res.json(favorite);
+});
+
+app.delete("/api/delete-favorite/:id", async function(req, res){
+  const idfav = req.body.id;
+  const response = {isDeleted : true};
+  try{
+    const favorite = await deleteFavorite(idfav);
+    response.message = "Succeed in deleting Favorite !";
+    res.status(200).json(response);
+  }catch (err){
+    req.log(err);
+    response.message = "Error delete Favorite   \n" + err;
+    response.isDeleted = false;
+    res.status(500).json(response);
+  }
 });
 
 app.get("*", (req, res) => {
