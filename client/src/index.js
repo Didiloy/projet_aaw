068eb60782089a5c26e8b170ae7c61adcee6c031
dylan <didiloy2001@gmail.com>
2023-11-10 16:41:01 +0100

    add Citation page and citation component add login route and login component deleted tsconfig.json

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import App from "./App.js"; //homepage
+import Citations from "./routes/Citations.js";
+import Login from "./routes/Login.js";
 
 const AppRouting = () => (
   <Router>
     <Routes>
       <Route exact path="/" element={<App />} />
-      <Route exact path="/citations" element={<App />} />
-      <Route exact path="/login" element={<App />} />
+      <Route exact path="/citations" element={<Citations />} />
+      <Route exact path="/login" element={<Login />} />
     </Routes>
   </Router>
 );
