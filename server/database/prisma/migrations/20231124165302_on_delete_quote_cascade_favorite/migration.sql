e107908687bde8cfe71d09196eace6472a6750b6
dylan <didiloy2001@gmail.com>
2023-11-24 17:55:19 +0100

    updated database schema to allow the deletion of a quote which is favorited by a user

---
+++ server/database/prisma/migrations/20231124165302_on_delete_quote_cascade_favorite/migration.sql
@@ -0,0 +1,14 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Favorite" (
+    "userId" TEXT NOT NULL,
+    "quoteId" INTEGER NOT NULL,
+    CONSTRAINT "Favorite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("username") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Favorite_quoteId_fkey" FOREIGN KEY ("quoteId") REFERENCES "Quote" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Favorite" ("quoteId", "userId") SELECT "quoteId", "userId" FROM "Favorite";
+DROP TABLE "Favorite";
+ALTER TABLE "new_Favorite" RENAME TO "Favorite";
+CREATE UNIQUE INDEX "Favorite_userId_quoteId_key" ON "Favorite"("userId", "quoteId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
