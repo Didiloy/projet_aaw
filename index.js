61d8d11157be56d2938f2fe146329c121f4fe105
dylan <didiloy2001@gmail.com>
2024-01-03 14:30:51 +0100

    changed redirect uri

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(cookieParser());
 //=======Authentication=======
 app.get("/auth/discord/login", async (req, res) => {
   const url =
-    "https://discord.com/api/oauth2/authorize?client_id=1172524572537532529&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify";
+    "https://discord.com/api/oauth2/authorize?client_id=1172524572537532529&redirect_uri=%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify";
   res.redirect(url);
 });
 
